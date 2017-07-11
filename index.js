8adb9fb6bfdf0f2458310442c367789ffa918d9d
Derek Miranda <derek.miranda.1992@gmail.com>
2017-07-11 11:16:07 -0700

    Initiated project

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
