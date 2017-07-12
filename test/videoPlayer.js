b63d9e8d4cd90d43eb67b8edb9a94af0a331cd9d
Derek Miranda <derek.miranda.1992@gmail.com>
2017-07-11 19:39:05 -0700

    Fixed test

--- test/videoPlayer.js
+++ test/videoPlayer.js
@@ -4,7 +4,7 @@ const assert = require('chai').assert;
 describe('Video Player', () => {
   describe('True', () => {
     it("should be true if it's true", () => {
-      assert.equal(true, false);
+      assert.equal(true, true);
     })
   })
 });
