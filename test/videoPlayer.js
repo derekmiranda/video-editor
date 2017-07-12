ea43d369e8dac398e6742f0089cfd4ee7b4fd702
Derek Miranda <derek.miranda.1992@gmail.com>
2017-07-11 20:38:31 -0700

    Removed zombie, considering other headless browser w/ selection

--- test/videoPlayer.js
+++ test/videoPlayer.js
@@ -1,10 +1,7 @@
 const assert = require('chai').assert;
-// const casper = require('casperjs');
 
 describe('Video Player', () => {
-  describe('True', () => {
-    it("should be true if it's true", () => {
-      assert.equal(true, true);
-    })
-  })
+  it("Timeline should be same width as video", () => {
+    assert.equal(true, true);
+  });
 });
