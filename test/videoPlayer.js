b2bb1fa92982365521979c891e866ec05cecc9ec
Derek Miranda <derek.miranda.1992@gmail.com>
2017-07-11 17:42:26 -0700

    Added video player test file

---
+++ test/videoPlayer.js
@@ -0,0 +1,9 @@
+const assert = require('chai').assert;
+
+describe('Video Player', () => {
+  describe('True', () => {
+    it("should be true if it's true", () => {
+      assert.equal(true, true);
+    })
+  })
+});
