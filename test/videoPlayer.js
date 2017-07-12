d9e3f6f78429c493e0c10988de1216d4047820d8
Derek Miranda <derek.miranda.1992@gmail.com>
2017-07-11 18:03:46 -0700

    Failing test

--- test/videoPlayer.js
+++ test/videoPlayer.js
@@ -1,9 +1,10 @@
 const assert = require('chai').assert;
+const casper = require('casperjs');
 
 describe('Video Player', () => {
   describe('True', () => {
     it("should be true if it's true", () => {
-      assert.equal(true, true);
+      assert.equal(true, false);
     })
   })
 });
