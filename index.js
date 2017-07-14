2e0a5c59b5c9dd507f5b886e78e6a7ac1ede3310
Derek Miranda <derek.miranda.1992@gmail.com>
2017-07-13 23:32:03 -0700

    Laid out some presentation tests for video player, unsuccessfully tried zombie for front-end testing

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const { PORT } = require('./shared');
 
 app.use(express.static('public'));
 
--- test/videoPlayer.js
+++ test/videoPlayer.js
@@ -1,7 +1,33 @@
 const assert = require('chai').assert;
 
+// start server
+require('..');
+const { PORT } = require('../shared');
+
+
 describe('Video Player', () => {
-  it("Timeline should be same width as video", () => {
-    assert.equal(true, true);
+  const browser = new Browser();
+  browser.silent = true;
+
+  before((done) => {
+    browser.visit(`http://localhost:${PORT}/`, done);
+  });
+
+  describe('Presentation', function() {
+    it('loads successfully', function() {
+      // browser.assert.success('Unsuccessful load');
+      // assert.equal(browser.tabs.length, 1, 'No tab showing');
+    });
+
+    it('has a timeline that is the same width as the video', function() {
+      // const htmlDoc = browser.window.document;
+      // const video = htmlDoc.querySelector('video');
+      // const timeline = htmlDoc.querySelector('.timeline');
+      
+      // const videoWidth = video.clientWidth;
+      // const timelineWidth = timeline.clientWidth;
+
+      // assert.equal(videoWidth, timelineWidth, 'Video and timeline not same width');
+    });
   });
 });
